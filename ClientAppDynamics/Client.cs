936acde60c8f78949b2cbb48b164e80be4bb9ce3
Diego <diego2.gd@gmail.com>
2020-01-02 23:36:15 +0100

    Last details - remote and local client

--- ClientAppDynamics/Client.cs
+++ ClientAppDynamics/Client.cs
@@ -11,25 +11,24 @@ namespace ClientAppDynamics
 {
     class Client
     {
-        private static string command = ""; 
+        private static string command = "";
 
         // Main Method 
         static void Main(string[] args)
         {
-
             ExecuteClient(args);
         }
 
-        // Test if input arguments were supplied.
-        private static bool isValidArgs(string[] args)
+        // Check if input arguments were supplied and valid.
+        private static bool areValidArgs(string[] args)
         {
-                   
+
             if (args.Length == 0)
             {
                 Console.WriteLine("Please enter a command.");
                 return false;
             }
-            
+
             if (args.Length > 2)
             {
                 Console.WriteLine("Limit of commands over. Try with HELLO, TIME or DIR /path.");
@@ -37,6 +36,7 @@ namespace ClientAppDynamics
             }
 
             command = string.Join(" ", args); //It sets the command if they were provided
+            Console.WriteLine("\nCommand introduced: " + command);
 
             return true;
 
@@ -45,56 +45,46 @@ namespace ClientAppDynamics
         // ExecuteClient() Method 
         static void ExecuteClient(string[] args)
         {
-            if (isValidArgs(args))
+            if (areValidArgs(args))
             {
                 try
                 {
 
-                    // Establish the remote endpoint 
-                    // for the socket. This example 
-                    // uses port 11111 on the local 
-                    // computer. 
+                    // Establish the remote endpoint for the socket. 
+                    //This example uses port 10000. 
                     IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
-                    IPAddress ipAddr = ipHost.AddressList[0];
-                    IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+                    IPAddress ipAddr = ipHost.AddressList[0]; //IPAddress.Parse("51.140.222.237") for introducing a specific IP
+                    IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 10000);
 
-                    // Creation TCP/IP Socket using 
-                    // Socket Class Costructor 
+                    // Creation TCP/IP Socket using Socket Class Costructor 
                     Socket sender = new Socket(ipAddr.AddressFamily,
                             SocketType.Stream, ProtocolType.Tcp);
 
                     try
                     {
 
-                        // Connect Socket to the remote 
-                        // endpoint using method Connect() 
+                        // Connect Socket to the remote endpoint using method Connect() 
                         sender.Connect(localEndPoint);
 
-                        // We print EndPoint information 
-                        // that we are connected 
-                        Console.WriteLine("Socket connected to -> {0} ",
+                        // We print EndPoint information that we are connected 
+                        Console.WriteLine("Socket connected to {0} ",
                                     sender.RemoteEndPoint.ToString());
 
-                        // Creation of messagge that 
-                        // we will send to Server 
-                        byte[] messageSent = Encoding.ASCII.GetBytes(command + "<EOF>");
+                        // Creation of messagge that we will send to Server
+                        byte[] messageSent = Encoding.ASCII.GetBytes(command + "<EOF>"); // EOF: End of file
                         int byteSent = sender.Send(messageSent);
 
                         // Data buffer 
                         byte[] messageReceived = new byte[1024];
 
-                        // We receive the messagge using 
-                        // the method Receive(). This 
-                        // method returns number of bytes 
-                        // received, that we'll use to 
-                        // convert them to string 
+                        // We receive the messagge using the method Receive(). 
+                        // This method returns number of bytes received, that we'll use to convert them to string 
                         int byteRecv = sender.Receive(messageReceived);
-                        Console.WriteLine("Message from Server -> {0}",
+                        Console.WriteLine("\nMessage from Server -> \n{0}",
                             Encoding.ASCII.GetString(messageReceived,
                                                         0, byteRecv));
 
-                        // Close Socket using 
-                        // the method Close() 
+                        // Close Socket using the method Close() 
                         sender.Shutdown(SocketShutdown.Both);
                         sender.Close();
                     }
@@ -109,7 +99,7 @@ namespace ClientAppDynamics
                     catch (SocketException se)
                     {
 
-                        Console.WriteLine("SocketException : {0}", se.ToString());
+                        Console.WriteLine("SocketException " + se.ErrorCode + " : {0}", "Connection cant be established, the server denied the connection");
                     }
 
                     catch (Exception e)
